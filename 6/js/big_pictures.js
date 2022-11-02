8ea738f8e28d88ea6668a6a160c0644d12727454
Keks <keks@htmlacademy.ru>
2022-11-02 21:42:20 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/big_pictures.js
@@ -0,0 +1,68 @@
+/* eslint-disable no-unused-vars */
+
+function openPicture(picture) {
+  const bigPicture = document.querySelector('.big-picture');
+  bigPicture.classList.remove('hidden');
+
+  document.body.classList.add('.modal-open');
+
+  const img = bigPicture.querySelector('.big-picture__img').querySelector('img');
+  const likes = bigPicture.querySelector('.likes-count');
+
+  const commentTemplate = document.querySelector('#comment').content.querySelector('.social__comment');
+  const commentsContainer = bigPicture.querySelector('.social__comments');
+  const commentsCount = bigPicture.querySelector('.comments-count');
+  const caption = bigPicture.querySelector('.social__caption');
+
+  const commentCounter = bigPicture.querySelector('.social__comment-count');
+  const commentsLoader = bigPicture.querySelector('.comments-loader');
+
+  const pictureCloseButton = bigPicture.querySelector('#picture-cancel');
+
+  pictureCloseButton.addEventListener('click', (evt) => {
+    evt.preventDefault();
+
+    document.body.classList.remove('.modal-open');
+    bigPicture.classList.add('hidden');
+  });
+
+  const onEscClickEvent = (evt) => {
+    evt.preventDefault();
+
+    if (evt.key === 'Escape') {
+      document.body.classList.remove('.modal-open');
+      bigPicture.classList.add('hidden');
+
+      document.removeEventListener('keydown', onEscClickEvent);
+    }
+  };
+
+  document.addEventListener('keydown', onEscClickEvent);
+
+  commentCounter.classList.add('hidden');
+  commentsLoader.classList.add('hidden');
+
+  img.src = picture.url;
+  likes.textContent = picture.likes;
+  caption.textContent = picture.description;
+  commentsCount.textContent = picture.comments.length.toString();
+
+  const similarCommentsFragment = document.createDocumentFragment();
+
+  const commentList = commentsContainer.querySelectorAll('.social__comment');
+  commentList.forEach((comment) => comment.remove());
+
+  picture.comments.forEach((comment) => {
+    const commentElement = commentTemplate.cloneNode(true);
+    const commentPicture = commentElement.querySelector('.social__picture');
+    const commentText = commentElement.querySelector('.social__text');
+
+    commentPicture.src = comment.avatar;
+    commentPicture.alt = comment.name;
+    commentText.textContent = comment.message;
+    similarCommentsFragment.appendChild(commentElement);
+  });
+  commentsContainer.appendChild(similarCommentsFragment);
+}
+
+export {openPicture};
